9d10710f1f05c82c7c6f63bef58c9211c907e327
Alexandre Soares <alexandre0610@gmail.com>
2025-07-26 18:03:59 -0300

    Adiciona avisos a agenda

--- src/Components/Agenda.tsx
+++ src/Components/Agenda.tsx
@@ -1,18 +1,42 @@
 import Calendario from "./Calendario";
 function Agenda() {
   return (
-    <section
-      id="calendario"
-      className="flex flex-col md:flex-row items-center max-w-7xl mx-auto gap-8 py-16">
-      <div className="flex flex-col items-end md:max-w-1/2">
-        <h1 className="font-bold text-6xl text-center md:text-end text-branco px-4 ">
-          Confira a nossa agenda{" "}
-        </h1>
-        <p className="mt-4 text-2xl text-center md:text-end text-branco px-4">
-          Nossas gíras acontecem as terças feiras, sempre as 19:30h.
-        </p>
+    <section id="calendario" className="flex flex-col mx-auto gap-10 py-16">
+      <h1 className="font-bold text-6xl text-center text-branco">
+        Confira a nossa agenda{" "}
+      </h1>
+      <div className="flex flex-col md:flex-row justify-center mx-auto gap-14 px-4">
+        <Calendario></Calendario>
+        <div className="flex md:max-w-1/3 text-2xl gap-4 text-branco">
+          {/* <h3 className="text-4xl text-center">Orientações</h3> */}
+          <ul className="list-disc pl-6 text-xl text-branco/90 flex flex-col gap-4">
+            <li>
+              O terreiro é um local sagrado, evite roupas curtas, decoatadas ou
+              escuras. Recomenda-se roupas claras e sem muitos acessórios que
+              possam chamar atenção.
+            </li>
+            <li>Ao chegar ao terreiro, coloque o seu celular no silencioso.</li>
+            <li>
+              É recomendado ficar descalço para que se conecte melhor e sinta a
+              energia.
+            </li>
+            <li>
+              Atente-se ao chamado das senhas para tomar o seu passe e, ao ser
+              chamado devolva a ficha ao médium responsável.
+            </li>
+            <li>
+              Não é permitido fumar nas dependências do terreiro, somente na
+              área externa
+            </li>
+            <li>
+              Não é permitido filmar ou fotografar dentro das dependências do
+              terreiro.
+            </li>
+            <li>Contribua com a limpeza, lixo é no lixo.</li>
+            <li>Em caso de dúvidas, pergunte aos médiuns presentes</li>
+          </ul>
+        </div>
       </div>
-      <Calendario></Calendario>
     </section>
   );
 }
--- src/Components/giras.ts
+++ src/Components/giras.ts
@@ -2,68 +2,100 @@ export interface Gira {
   titulo: string;
   day: number;
   month: string;
+  year: number;
   monthIndex: number;
 }
 
 export const giras: Array<Gira> = [
   {
+    titulo: "Gira com Pretos Velhos",
+    day: 3,
+    month: "Junho",
+    year: 2025,
+    monthIndex: 6,
+  },
+  {
+    titulo: "Gira com Exus e Pomba Giras",
+    day: 10,
+    month: "Junho",
+    year: 2025,
+    monthIndex: 6,
+  },
+  {
+    titulo: "Gira com Caboclos e Boiadeiros",
+    day: 17,
+    month: "Junho",
+    year: 2025,
+    monthIndex: 6,
+  },
+  {
+    titulo: "Gira com Caboclos / Dia de Xangô",
+    day: 24,
+    month: "Junho",
+    year: 2025,
+    monthIndex: 6,
+  },
+  {
     titulo: "Aniversário de 1 Ano do Centro",
     day: 1,
     month: "Julho",
+    year: 2025,
     monthIndex: 7,
   },
   {
     titulo: "Gira com Pretos Velhos",
     day: 8,
     month: "Julho",
+    year: 2025,
     monthIndex: 7,
   },
   {
     titulo: "Gira com Caboclos e Boiadeiros",
     day: 15,
+    year: 2025,
     month: "Julho",
     monthIndex: 7,
   },
   {
     titulo: "Gira com Pretos Velhos",
     day: 22,
+    year: 2025,
     month: "Julho",
     monthIndex: 7,
   },
   {
     titulo: "Gira com Caboclos",
     day: 29,
+    year: 2025,
     month: "Julho",
     monthIndex: 7,
   },
   {
-    titulo: "Aniversário de 1 Ano do Centro",
-    day: 1,
+    titulo: "Gira com Exus e Pomba Giras",
+    day: 5,
     month: "Agosto",
+    year: 2025,
     monthIndex: 8,
   },
   {
-    titulo: "Gira com Pretos Velhos",
-    day: 8,
+    titulo: "Gira Caboclos e Boiadeiros",
+    day: 12,
     month: "Agosto",
-    monthIndex: 8,
-  },
-  {
-    titulo: "Gira com Caboclos e Boiadeiros",
-    day: 15,
-    month: "Agosto",
-    monthIndex: 8,
-  },
-  {
-    titulo: "Gira com Pretos Velhos",
-    day: 22,
-    month: "Agosto",
+    year: 2025,
     monthIndex: 8,
   },
   {
     titulo: "Gira com Caboclos",
-    day: 29,
+    day: 19,
     month: "Agosto",
+    year: 2025,
+    monthIndex: 8,
+  },
+  {
+    titulo: "Gira com Pretos Velhos",
+    day: 26,
+    month: "Agosto",
+    year: 2025,
     monthIndex: 8,
   },
 ];
